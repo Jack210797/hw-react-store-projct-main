2a5fd643461d6220ab9ec10ce2915c6db8651263
Jack210797 <suvorov21079@gmail.com>
2024-08-29 16:22:55 +0300

    2

--- src/types/Product.Interface.ts
+++ src/types/Product.Interface.ts
@@ -1,10 +1,11 @@
 export interface ProductInterface {
-  length: number
-  map(arg0: (product: ProductInterface) => import('react/jsx-runtime').JSX.Element): import('react').ReactNode
   id: string
   name: string
   price: number
   description: string
   category: string
   image: string
+
+  length: number
+  map: any
 }
